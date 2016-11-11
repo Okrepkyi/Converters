11024ff88bd23019ec6795fb06e594bc7caa5a3d
Oleksandr <o.okrepkyi@gmail.com>
2016-11-11 09:35:42 +0200

    Delete GeoCalculator_to_ASCII.py

--- GeoCalculator_to_ASCII.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-filename = input()
-with open(filename, "r") as f:
-    sys.stdout = open('%s (converted).txt' % f.name, 'a')
-    for line in f:
-        splitted = line.split(',')
-        print('\t'.join(splitted[1:3]))
